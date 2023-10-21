35db838b71b1c8610ec14ad98596743967513526
Wladimir Rocha <wldrocha@gmail.com>
2023-10-21 10:27:52 -0400

    [ADD] conditional render on main

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,15 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { TodoProvider } from './context/TodoContext.tsx'
+const rootElement = document.getElementById('root')
 
-createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+if (rootElement != null) {
+  createRoot(rootElement).render(
+    <React.StrictMode>
+      <TodoProvider>
+        <App />
+      </TodoProvider>
+    </React.StrictMode>
+  )
+}
