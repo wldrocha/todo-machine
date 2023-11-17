35582f53eede88c968237d4fc18015a4119276db
Wladimir Rocha <wld.rocha@gmail.com>
2023-11-17 11:31:10 -0400

    [REF] routes ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import viteTsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: 'https://wldrocha.github.io/todo-machine/',
-  plugins: [react()]
+  plugins: [react(), viteTsconfigPaths()]
 })
