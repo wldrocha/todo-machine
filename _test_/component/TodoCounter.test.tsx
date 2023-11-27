39c1161b8caf1d9aa8bbf6096af321c0a838ffdb
Wladimir Rocha <wld.rocha@gmail.com>
2023-11-27 11:58:06 -0400

    [REF] change on test

--- _test_/component/TodoCounter.test.tsx
+++ _test_/component/TodoCounter.test.tsx
@@ -7,12 +7,12 @@ import { TodoCounter } from '../../src/components/TodoCounter'
 describe('TodoCounter', () => {
   it('should render component', () => {
     render(<TodoCounter completedTodos={0} totalTodos={0} />)
-    
+
     expect(screen.queryByText('Has completado '))
   })
   it('should render completed todos and total todos', () => {
     render(<TodoCounter completedTodos={1} totalTodos={3} />)
-    
+
     expect(screen.queryByText('Has completado 1 de 3 TODOS'))
   })
 })
--- _test_/hooks/useLocalsstorage.test.ts
+++ _test_/hooks/useLocalsstorage.test.ts
@@ -1,68 +1,42 @@
 // useLocalStorage.test.ts
-import { expect, it, describe, afterEach } from 'vitest'
+import { expect, it, describe, vi, afterEach } from 'vitest'
 import { renderHook, act, cleanup } from '@testing-library/react-hooks'
 import { useLocalStorage } from '../../src/hooks'
+import { Todo } from '../../src/types'
 
 describe('useLocalStorage', () => {
-  const initValue = []
-  const itemName = 'TODOS_V1'
-  const temSaveValue = { title: 'test', completed: false, id: '3cbae6a1-16e3-46be-9cdf-71381b136a48' }
-  const itemSaveArrayValue = [temSaveValue]
-
-  afterEach(() => {
-    cleanup()
-  })
-  it('should initialize with initialValue', () => {
-    const { result } = renderHook(() => useLocalStorage(itemName, initValue))
-    const { item } = result.current
-    expect(item).toEqual(initValue)
+  it('should initialize with initial value if no value is in local storage', () => {
+    const { result } = renderHook(() => useLocalStorage('test', 'initial'))
+    expect(result.current.item).toEqual('initial')
   })
 
-  // it('should update item when localStorage item is updated', () => {
-  //   const updateValue = [itemValueToSave]
-  //   const { result } = renderHook(() => useLocalStorage(itemName, initValue))
-  //   const { item } = result.current
+  it('should save a new item', () => {
+    const { result } = renderHook(() => useLocalStorage('test', 'initial'))
+    act(() => {
+      result.current.saveItem('newItem')
+    })
+    expect(result.current.item).toEqual('newItem')
+  })
 
-  //   localStorage.setItem(itemName, JSON.stringify(updateValue))
-  //   act(() => {}) // Simulate rerender
+  // it('should synchronize item when storage event is fired', () => {
+  //   const { result } = renderHook(() => useLocalStorage('test', 'initial'))
 
-  //   // expect(item).toEqual(updateValue)
+  //   act(() => {
+  //     window.dispatchEvent(new StorageEvent('storage', { key: 'test' }))
+
+  //     result.current.sincroniceItem()
+  //   })
+  //   console.log("🚀 ~ file: useLocalsstorage.test.ts:28 ~ act ~ result.current.:", result.current)
+    
   // })
 
-  // it('should handle errors when parsing localStorage item', () => {
-  //   const { result } = renderHook(() => useLocalStorage('itemName', 'initialValue'))
-  //   const { error, getInfoOnLocalStorage } = result.current
+  it('should handle loading state', () => {
+    const { result } = renderHook(() => useLocalStorage('test', 'initial'))
+    expect(result.current.loading).toEqual(true)
+  })
 
-  //   localStorage.setItem('itemName', 'invalidJSON')
-  //   act(() => getInfoOnLocalStorage())
-
-  //   expect(error.error).toBeTruthy()
-  //   expect(error.message).toEqual('Ocurrió un error')
-  // })
-
-  // it('should save item to localStorage', () => {
-  //   const { result } = renderHook(() => useLocalStorage(itemName, initValue))
-  //   const { saveItem } = result.current
-
-  //   saveItem(temSaveValue)
-  //   expect(localStorage.getItem('itemName')).toEqual(JSON.stringify(itemSaveArrayValue))
-  // })
-
-  // it('should set loading to true when sincroniceItem is called', () => {
-  //   const { result } = renderHook(() => useLocalStorage('itemName', 'initialValue'))
-  //   const { sincroniceItem, loading } = result.current
-
-  //   sinconiceItem()
-  //   expect(loading).toBeTruthy()
-  // })
-
-  // it('should set loading to false when sincroniceItem is called and localStorage item is updated', () => {
-  //   const { result } = renderHook(() => useLocalStorage('itemName', 'initialValue'))
-  //   const { sincroniceItem, loading } = result.current
-
-  //   sinconiceItem()
-  //   act(() => {}) // Simulate rerender
-
-  //   expect(loading).toBeFalsy()
-  // })
+  it('should handle error state', () => {
+    const { result } = renderHook(() => useLocalStorage('test', 'initial'))
+    expect(result.current.error).toEqual({ error: false, message: '' })
+  })
 })
